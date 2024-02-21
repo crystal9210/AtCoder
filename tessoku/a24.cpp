2a5e94c4c3572c685adfd123e95aad914fab88de
crystal9210 <sy9210sss@yahoo.co.jp>
2024-02-22 03:38:01 +0900

    add comments

--- tessoku/a24.cpp
+++ tessoku/a24.cpp
@@ -5,7 +5,7 @@ using namespace std;
 typedef long long ll;
 typedef long double lb;
 
-const int INF=1<<30;  // 2^30
+const int INF=1<<30;  // 2^30；初期化、比較の時に「到達不可能」「非常に大きな値」として使う
 
 int main(){
   int n,max=0,cnt=0;
@@ -14,12 +14,24 @@ int main(){
   rep(i,0,n)cin>>a[i];
   rep(i,0,n){
     // 注意:dp.begin()はイテレータであり、値ではない!+イテレータ間の差分は、コンテナの要素数に等しい→下記のようにして算出
-    auto idx=lower_bound(dp.begin(),dp.end(),a[i])-dp.begin();
+    auto idx=lower_bound(dp.begin(),dp.end(),a[i])-dp.begin();  // lower_bound():イテレータの範囲内から指定した値以上の最小の要素を探し、その要素を指すイテレータを返す
     dp[idx]=a[i];
+    for (int i = 0; i < n + 1; ++i) {
+      if (dp[i] < INF) {
+          cout << dp[i] << " ";
+      } else {
+          break; // INFに到達したら、それ以上の要素はLISに関連しないため、ループを抜ける
+      }
+      }
+    cout << endl;
   }
+
+
 
   auto ans=lower_bound(dp.begin(),dp.end(),INF) - dp.begin();
   cout<<ans<<endl;
+
+
 
   return 0;
 
--- tessoku/a26.cpp
+++ tessoku/a26.cpp
@@ -5,24 +5,37 @@ using namespace std;
 typedef long long ll;
 typedef long double lb;
 
-// TLEする→素数判定において、時間を節約するためにはエラトステネスの篩(Slave of Eratosthenes)が有効
+// TLEする→素数判定において、時間を節約するためにはエラトステネスの篩(ふるい)(Slave of Eratosthenes)が有効
 vector<bool> sieve(int n){
-  vector<bool> prime(n+1, true);
+  vector<bool> prime(n+1, true);  // 初期状態ではすべての数が素数と仮定している
+  prime[0] = prime[1] = false; // 0と1は素数ではない
+  // 2から始めてnの平方根までの範囲を検討→検討範囲を大幅に小さくする(これでいいらしい)
+    for(int i = 2; i*i <= n; ++i) {
+      // iが素数のときのみ実行するようにすることで正確に非素数を排除
+        if(prime[i]) {
+            for(int j = i*i; j <= n; j += i) {
+                prime[j] = false; // iの倍数を非素数とマーク
+            }
+        }
+    }
+    return prime;
 }
 
+// この関数を利用するとTLEとなり×
 // 素数判定をするための関数
-bool isPrime(int n){
-  rep(i,2,n)if(n%i==0)return false;
-  return true;
-}
+// bool isPrime(int n){
+//   rep(i,2,n)if(n%i==0)return false;
+//   return true;
+// }
 
 int main(){
   int q;
   cin>>q;
+  vector<bool> prime=sieve(300000);
   rep(i,0,q){
     int x;
     cin>>x;
-    if(isPrime(x)){
+    if(prime[x]){
       cout<<"Yes"<<endl;
     }else{
       cout<<"No"<<endl;
